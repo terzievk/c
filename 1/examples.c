bd4bfef4ea18487395e3e5c1a014182b1021e9aa
Konstantin Terziev <terzievk@yahoo.com>
2024-08-24 13:57:01 +0300

    update the new example

--- 1/examples.c
+++ 1/examples.c
@@ -3,54 +3,59 @@
 
 #define MAXLINE  1000   /* maximum input line size */
 
-int getLine(char line[], int maxLine);
-void copy(char to[], char from[]);
+int max;   /* maximum length seen so far */
+char line[MAXLINE];     /* current input line */
+char longest[MAXLINE];  /* longest line saved here */
 
-/* print longest input line */
+int getLine(void);
+void copy(void);
+
+/* print longest input line; specialized version */
 int main() {
   int len;   /* current line length */
-  int max;   /* maximum length seen so far */
-  char line[MAXLINE];     /* current input line */
-  char longest[MAXLINE];  /* longest line saved here */
+  extern int max;
+  extern char longest[];
 
   max = 0;
-  while ((len = getLine(line, MAXLINE)) > 0) {
+  while ((len = getLine()) > 0) {
     if (len > max) {
       max = len;
-      copy(longest, line);
+      copy();
     }
   }
 
-  if (max > 0) {
+  if (max > 0) {  /* there was a line */
     printf("longest: %s", longest);
   }
 
   return 0;
 }
 
-/* getline: read a line into s, return length */
-int getLine(char s[], int lim) {
+/* getline: specialized version */
+int getLine() {
   int c, i;
+  extern char line[];
 
-  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-    s[i] = c;
+  for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    line[i] = c;
   }
 
   if (c == '\n') {
-    s[i] = c;
+    line[i] = c;
     ++i;
   }
 
-  s[i] = '\0';
+  line[i] = '\0';
   return i;
 }
 
-/* copy: copy `from` into `to`; assume to is a big enough */
-void copy(char to[], char from[]) {
+/* copy: specialized version */
+void copy() {
   int i;
+  extern char line[], longest[];
 
   i = 0;
-  while ((to[i] = from[i]) != '\0') {
+  while ((longest[i] = line[i]) != '\0') {
     ++i;
   }
 }
