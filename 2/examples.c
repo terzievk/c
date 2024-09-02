1bb5e52762c0b7cb932cc4a737d43e3f7a7c49ab
Konstantin Terziev <terzievk@yahoo.com>
2024-09-02 15:45:41 +0300

    new example from chapter 2.3

--- 2/examples.c
+++ 2/examples.c
@@ -1,9 +1,21 @@
 // copyright
 #include <stdio.h>
 
+/* strlen: return length of s */
+int strlen(char s[]) {
+  int i;
+
+  i = 0;
+  while (s[i]) {
+    ++i;
+  }
+
+  return i;
+}
+
 int main() {
-  //  printf("%d\n", 234f);
-  printf("%d\n", 234ul);
-  printf("%d\n", 234L);
+  char s[100] = "hello" " " "world\0";
+  printf("%s: %d\n", s, strlen(s));
+
   return 0;
 }
